1b8c14851b6a904ce7f1dab2c89bed44d141eb3f
Chris Postma <cjrpostma@gmail.com>
2020-07-13 09:57:46 -0400

    Add markers to map.

---
+++ src/CustomMap.ts
@@ -0,0 +1,39 @@
+// instructions to other classes on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hello',
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
