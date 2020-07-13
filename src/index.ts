099f9fc5800c44e464902e47541f099aba439841
Chris Postma <cjrpostma@gmail.com>
2020-07-12 20:39:59 -0400

    Add Google maps script and map instance.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { User } from './User';
+import { Company } from './Company';
 
-const user = new User();
-console.log(user);
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
